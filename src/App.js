e03a7c4c144078dc66d14cc28265368e3f431f24
Sastry <pssastry@38f9d34e6b94.ant.amazon.com>
2021-04-07 08:31:12 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Form, Table, Container } from "react-bootstrap";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = () => {
+  const [files, setFiles] = useState("");
+
+  const createObject = (data) => {
+    const lines = data.split("\n");
+    const headers = lines[0].split(",");
+
+    const result = [];
+    for (let i = 1; i < lines.length; i++) {
+      let obj = {};
+      var currentline = lines[i].split(",");
+
+      for (var j = 0; j < headers.length; j++) {
+        obj[headers[j]] = currentline[j];
+      }
+      result.push(obj);
+    }
+
+    return result;
+  };
+
+  const handleChange = (e) => {
+    const fileReader = new FileReader();
+    fileReader.readAsText(e.target.files[0], "UTF-8");
+    fileReader.onload = (e) => {
+      setFiles(e.target.result);
+    };
+  };
+
+  const data = createObject(files);
+  let headers = data.length > 0 ? Object.keys(data[0]) : [];
+
+  const tableHeader = (
+    <thead>
+      <tr>
+        {headers.map((h) => (
+          <th key={h}>{h}</th>
+        ))}
+      </tr>
+    </thead>
   );
-}
+
+  const tableData = data.map((d, i) => {
+    return (
+      <tr key={i}>
+        {headers.map((h) => (
+          <td key={d[h]}>{d[h]}</td>
+        ))}
+      </tr>
+    );
+  });
+
+  const T = (
+    <Table striped bordered hover>
+      {tableHeader}
+      <tbody>{tableData}</tbody>
+    </Table>
+  );
+
+  return (
+    <Container>
+      <h1>Example File Upload</h1>
+
+      <Form>
+        <Form.File
+          id="custom-file"
+          label="Custom file input"
+          custom
+          onChange={handleChange}
+        />
+      </Form>
+      <hr />
+      <h3>Data uploaded</h3>
+      {T}
+    </Container>
+  );
+};
 
 export default App;
